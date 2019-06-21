9b4c61dec2d76c5f15dc2d65627af88c39441816
t3rmian <t3rmian@gmail.com>
2019-06-21 19:21:41 +0200

    Initial commit

---
+++ app/src/androidTest/java/t3rmian/github/io/travis_android_demo/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package t3rmian.github.io.travis_android_demo
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("t3rmian.github.io.travis_android_demo", appContext.packageName)
+    }
+}
